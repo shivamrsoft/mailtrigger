cd3ef5f07e9d90ef6245082a74744a2777539836
shivamrsoft <shivamrsoft@gmail.com>
2022-02-16 13:35:45 +0530

    API health check

---
+++ APIServiceMailTrigger.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3.8
+
+import json
+from socket import timeout
+import requests
+
+
+from mailFn import mail_notification
+
+
+# Service Specific Health Check
+def dockerComposeServiceHealthCheckFn():
+    # servicename, container_name, url
+    servicedata = ''
+    body = []
+    with open("./apiservicedata.json") as f:
+        data = json.load(f)
+        for service in data:
+            try:
+                servicedata = data[service]
+                urlresponse = requests.get(servicedata['url'], timeout=1)
+                datanew = urlresponse.json()
+                servicedata['message'] = datanew['message']
+                servicedata['statusCode'] = datanew['statusCode']
+                print(datanew)
+                body.append(servicedata)
+            except:
+                print('1')
+                servicedata['message'] = 'Container is down'
+                servicedata['statusCode'] = 500
+                body.append(servicedata)
+        # mail_notification(body)
+
+
+if __name__ == '__main__':
+    dockerComposeServiceHealthCheckFn()
---
+++ mailFn.py
@@ -0,0 +1,89 @@
+from email.mime.application import MIMEApplication
+import subprocess
+from tabulate import tabulate
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP, SMTPException
+import os
+from configparser import ConfigParser
+
+# mail trigger and configuration function
+
+
+def mail_notification(body):
+    config = ConfigParser()
+    config.read('.env')
+    SMTP_PORT = config['mailconfig']['SMTP_PORT']
+    SMTP_SENDER = config['mailconfig']['SMTP_SENDER']
+    SMTP_PASSWORD = config['mailconfig']['SMTP_PASSWORD']
+    SMTP_RECEIVERS = config['mailconfig']['SMTP_RECEIVERS']
+    SMTP_HOST = config['mailconfig']['SMTP_HOST']
+    SUBJECT = "Health Check for BMS Project"
+    message = []
+    # Setup the MIME
+    msgData = MIMEMultipart()
+    msgData['From'] = SMTP_SENDER
+    msgData['To'] = SMTP_RECEIVERS
+    msgData['Subject'] = SUBJECT  # The subject line
+
+    for d in body:
+        constainer_status = []
+
+        if (d != {} and d['statusCode'] != 200):
+            constainer_status.append(subprocess.getoutput(
+                'docker inspect -f "{0}" {1}'.format("{{ .State.Status }}", d["container_name"])))
+            logs = [subprocess.getoutput(
+                f'docker logs --tail 100 {d["container_name"]}')]
+            with open("logs.txt", "a") as f:
+                f.writelines(logs)
+
+            # os.system(f'docker restart {d["container_name"]}')
+            newData = {
+                "Container Name": d["container_name"],
+                "Service Name":  d["service_name"],
+                "Response Code": d["statusCode"],
+                "Container Status": constainer_status[0],
+                "Message": f'Container {d["container_name"]} restarted successfully!',
+            }
+            message.append(newData)
+            with open("logs.txt", "rb") as fil:
+                part = MIMEApplication(
+                    fil.read(),
+                    Name="logs.txt"
+                )
+            # After the file is closed
+            part['Content-Disposition'] = 'attachment; filename="logs.txt"'
+            msgData.attach(part)
+
+        else:
+            constainer_status.append(subprocess.getoutput(
+                'docker inspect -f "{0}" {1}'.format("{{ .State.Status }}", d["container_name"])))
+            newData = {
+                "Container Name": d["container_name"],
+                "Service Name":  d["service_name"],
+                "Response Code": d["statusCode"],
+                "Container Status": constainer_status[0],
+                "Message": d['message']
+            }
+            message.append(newData)
+    bodyData = tabulate(message, headers="keys", showindex="always")
+
+    try:
+
+        msgData.attach(MIMEText(bodyData, 'plain'))
+
+        text = msgData.as_string()
+        print('Mail trigger started...')
+        with SMTP(SMTP_HOST, SMTP_PORT) as smtpObj:
+            # smtpObj = SMTP(SMTP_HOST, SMTP_PORT)
+            smtpObj.ehlo()
+            smtpObj.starttls()
+            smtpObj.ehlo()
+            print('Mail server connected!')
+            smtpObj.login(SMTP_SENDER, SMTP_PASSWORD)
+            print('Login Successful')
+            smtpObj.sendmail(SMTP_SENDER, SMTP_RECEIVERS, text)
+            smtpObj.quit()
+            print('Successfully sent email')
+    except SMTPException as err:
+        print('Error: unable to send email', err)
